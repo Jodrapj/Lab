df5bf91a0981e471fba57a168fef2a2bac4f5fe0
Jodrapj <ramen.ofmail@gmail.com>
2023-12-01 13:32:05 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Lab.Classes;
+using System.Windows;
+using Lab.Pages;
+
+namespace Lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameConnection.MainFrame = MFrame;
+            FrameConnection.MainFrame.Navigate(new Page1());
+        }
+    }
+}
